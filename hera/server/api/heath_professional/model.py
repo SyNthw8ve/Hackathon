fb5506c742cb5183782d5db2912a5e5da9897921
Nuno Carriço <nuno.mcarrico@decsis.pt>
2023-12-07 17:49:03 +0000

    Add back population to health professional

--- hera/server/api/heath_professional/model.py
+++ hera/server/api/heath_professional/model.py
@@ -1,8 +1,11 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from hera.server.database import Base
+from hera.server.api.pacient.model import PacientSQL
 
 class HealthProfessionalSQL(Base):
     __tablename__ = 'healthprofessional'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String)
+    patients = relationship('PacientSQL', back_populates='health_professional', lazy='selectin')
